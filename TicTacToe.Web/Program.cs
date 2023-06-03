7f8d60ea7ee6658d2b117d75f7bbfee2b59a1c7b
Mikhail Nazarov <nazarov23892@gmail.com>
2023-06-03 09:46:14 +0600

    added 'create new game' to api

--- TicTacToe.Web/Program.cs
+++ TicTacToe.Web/Program.cs
@@ -1,4 +1,6 @@
 using TicTacToe.Data.Game;
+using TicTacToe.Services.Game;
+using TicTacToe.Services.Game.Concrete;
 using TicTacToe.Services.Repositories;
 
 namespace TicTacToe.Web
@@ -11,10 +13,25 @@ namespace TicTacToe.Web
             var services = builder.Services;
 
             services.AddTransient<IGameRepository, InMemoryGameRepository>();
+            services.AddTransient<IGameService, GameService>();
 
             var app = builder.Build();
 
+            IGameService gameService;
+            using (var scope = app.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                gameService = serviceProvider
+                    .GetRequiredService<IGameService>();
+            }
+
             app.MapGet("/", () => "Hello World!");
+
+            // create new game
+            app.MapPost(
+                pattern: "/api/game/",
+                handler: () => gameService.Create()
+                );
 
             app.Run();
         }
