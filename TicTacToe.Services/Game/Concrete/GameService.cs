45dcce3189f0952104acbe8f665ccafaaefcc1f5
Mikhail Nazarov <nazarov23892@gmail.com>
2023-06-03 09:42:06 +0600

    added Game service

---
+++ TicTacToe.Services/Game/Concrete/GameService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Services.Entities;
+using TicTacToe.Services.Game.Responses;
+using TicTacToe.Services.Repositories;
+
+namespace TicTacToe.Services.Game.Concrete
+{
+    public class GameService : IGameService
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public GameService(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public CreateGameResponseDto Create()
+        {
+            GameState newGame = new GameState
+            {
+                GameId = Guid.NewGuid(),
+                Player1_Id = Guid.NewGuid(),
+                Status = GameStatus.WaitPlayer2_Connect
+            };
+            _gameRepository.AddGame(newGame);
+            return new CreateGameResponseDto
+            {
+                GameId = newGame.GameId,
+                Player1_Id = newGame.Player1_Id
+            };
+        }
+    }
+}
--- TicTacToe.Web/Program.cs
+++ TicTacToe.Web/Program.cs
@@ -1,3 +1,6 @@
+using TicTacToe.Data.Game;
+using TicTacToe.Services.Repositories;
+
 namespace TicTacToe.Web
 {
     public class Program
@@ -5,6 +8,10 @@ namespace TicTacToe.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var services = builder.Services;
+
+            services.AddTransient<IGameRepository, InMemoryGameRepository>();
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
